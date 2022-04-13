231ded3a9130afc3d45ca26106e8b1a88abb5cd7
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-13 16:26:10 -0700

    Complete Functional Pattern for  Stack function.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var numericKey = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[numericKey] = value;
+    numericKey++;
   };
-
+  debugger;
   someInstance.pop = function() {
+    delete storage[numericKey];
+    numericKey--;
+    return storage[numericKey];
   };
 
   someInstance.size = function() {
+    if (numericKey < 0) {
+      return 0;
+    } else {
+      return numericKey;
+    }
   };
 
   return someInstance;
