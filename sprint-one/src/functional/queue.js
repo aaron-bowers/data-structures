63dc06daefa5080c6fb8d3d72d50d66664b110b6
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-13 20:19:51 -0700

    Almost completed queue coding. Some final tests are failing.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,30 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  // create index tracking variable
+  var numericKey = 0;
+  var queueIndex = [];
 
+  // Implement the methods below
   someInstance.enqueue = function(value) {
+    storage[numericKey] = value;
+    queueIndex.push('"' + numericKey + '"');
+    numericKey++;
   };
 
   someInstance.dequeue = function() {
+    delete storage[queueIndex[0]];
+    queueIndex = queueIndex.slice(1);
+    numericKey--;
+    return storage[numericKey];
   };
 
   someInstance.size = function() {
+    if (numericKey < 0) {
+      return 0;
+    } else {
+      return numericKey;
+    }
   };
 
   return someInstance;
