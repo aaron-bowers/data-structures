c38b964ae663cc257da40ac868005b26cade395a
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-14 11:52:45 -0700

    Complete the Funcational-Shared Queue Problem

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,42 @@
+var storage, numericKey, sizeIndex;
+
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Initializations
+  storage = {};
+  numericKey = 0;
+  sizeIndex = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    storage[numericKey] = value;
+    numericKey++;
+    sizeIndex++;
+  },
+
+  dequeue: function() {
+    for (var key in storage) {
+      var firstInQueue = storage[key];
+      delete storage[key];
+      break;
+    }
+    sizeIndex--;
+    return firstInQueue;
+  },
+
+  size: function() {
+    if (sizeIndex < 0) {
+      return 0;
+    } else {
+      return sizeIndex;
+    }
+  }
+};
 
 
