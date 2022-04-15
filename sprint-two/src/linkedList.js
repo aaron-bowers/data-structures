62b65912956e4e18f3da1cae7712c23009944032
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-15 13:28:04 -0700

    Complete linked list challenge.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // declare variable and assign the return of the Node constructor
+    var newNode = Node(value);
+    // if there is a tail
+    if (this.tail) {
+      // add the instance to tail
+      this.tail.next = newNode;
+      this.tail = newNode;
+    // if there is no tail
+    } else {
+      // reassign tail to be node value
+      this.tail = newNode;
+      this.head = this.tail;
+    }
   };
 
   list.removeHead = function() {
+    // remove the head
+    var head = this.head;
+    // point head to next node
+    this.head = this.head.next;
+    // return the value of head variable
+    return head.value;
   };
 
   list.contains = function(target) {
+    // declare a variable named current
+    var current = this.head;
+    // iterate over the list starting from the head
+    while (current !== null) {
+      // if the current value is equal to the target
+      if (current.value === target) {
+        // return true
+        return true;
+      } else {
+      // if not, move to the next node
+        current = current.next;
+      }
+    }
+    // return boolean if list contains the target input
+    return false;
   };
 
   return list;
