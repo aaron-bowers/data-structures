965fe236036b3faae322d8c4f3dd16a2d3c4da19
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-15 13:28:21 -0700

    Complete tree challenge.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,33 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // base case
+  // if we find target
+  if (this.value === target) {
+    // return true
+    return true;
+  }
+  // if we don't find it AND there are no children
+  if (this.children.length === 0) {
+    // return false
+    return false;
+  }
+
+  // recursion
+  // not the target and has children
+  // iterate over each child
+  for (var i = 0; i < this.children.length; i++) {
+    // recursively call function on each child
+    if (this.children[i].contains(target)) {
+      return true;
+    }
+  }
+  // return false
+  return false;
 };
 
 
