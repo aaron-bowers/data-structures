cea674558a3666448a9361e83d9dffdffc0ba72d
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-14 17:30:59 -0700

    Refactor the code to include the this keyword before variables in both the constructor and methods.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,33 @@
+var storage, numericKey, sizeIndex;
+
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.numericKey = 0;
+    this.sizeIndex = 0;
   }
 
+  enqueue (value) {
+    this.storage[this.numericKey] = value;
+    this.numericKey++;
+    this.sizeIndex++;
+  }
+
+  dequeue () {
+    for (var key in this.storage) {
+      var firstInQueue = this.storage[key];
+      delete this.storage[key];
+      break;
+    }
+    this.sizeIndex--;
+    return firstInQueue;
+  }
+
+  size () {
+    if (this.sizeIndex < 0) {
+      return 0;
+    } else {
+      return this.sizeIndex;
+    }
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,28 @@
+var storage, numericKey;
+
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.numericKey = 0;
   }
 
+  push(value) {
+    this.storage[this.numericKey] = value;
+    this.numericKey++;
+  }
+
+  pop() {
+    delete this.storage[this.numericKey];
+    this.numericKey--;
+    return this.storage[this.numericKey];
+  }
+
+  size() {
+    if (this.numericKey < 0) {
+      return 0;
+    } else {
+      return this.numericKey;
+    }
+  }
 }
