706651258f1455519729067b4f85e70fd3d66fa3
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-15 17:23:05 -0700

    Complete the binary search tree problem.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,60 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(bstMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+
+  return tree;
 };
 
+var bstMethods = {};
+
+bstMethods.insert = function(value) {
+  var child = BinarySearchTree(value);
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = child;
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (!this.right) {
+      this.right = child;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+bstMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.value > target) {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(target);
+    }
+  }
+  if (this.value < target) {
+    if (!this.right) {
+      return false;
+    } else {
+      return this.right.contains(target);
+    }
+  }
+};
+
+bstMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
